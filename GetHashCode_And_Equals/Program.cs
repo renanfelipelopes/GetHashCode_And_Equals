f66032a82f473cb0b98250485b0255cbc26aa119
Renan Lopes <renanlopes.ads@gmail.com>
2021-11-03 20:40:02 -0300

    example of use

---
+++ GetHashCode_And_Equals/Program.cs
@@ -0,0 +1,19 @@
+﻿using GetHashCode_And_Equals.Entities;
+using System;
+
+namespace GetHashCode_And_Equals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "maria@gmail.com" };
+
+            Console.WriteLine(a.Equals(b)); //compara referencia de conteudo
+            Console.WriteLine(a == b); //compara referencia do ponteiro de memoria do objeto, como são objetos diferentes, vai dar falso
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
